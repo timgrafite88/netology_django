735bf498bdbbbd6ac375419a0f44b12cf97e6c10
Timur Pustoplesnov <timur.pustoplesnov@yandex.ru>
2024-10-28 22:20:58 +0300

    update 28-10-2024

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,21 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect, get_object_or_404
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
 
-
 def show_catalog(request):
+    # Получаем все телефоны из базы данных
+    phones = Phone.objects.all()
     template = 'catalog.html'
-    context = {}
+    # Передаем список телефонов в контекст
+    context = {'phones': phones}
     return render(request, template, context)
 
-
 def show_product(request, slug):
+    # Получаем телефон по slug (предполагается, что slug - это уникальное поле модели Phone)
+    phone = get_object_or_404(Phone, slug=slug)  # Используем get_object_or_404 для обработки ошибок
     template = 'product.html'
-    context = {}
+    # Передаем данные конкретного телефона в контекст
+    context = {'phone': phone}
     return render(request, template, context)
