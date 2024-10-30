a802408241c89c4d2ae192765dc4385a099d1c46
Timur Pustoplesnov <timur.pustoplesnov@yandex.ru>
2024-10-30 23:02:50 +0300

    update 30-10-2024

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -7,14 +7,27 @@ def index(request):
 def show_catalog(request):
     # Получаем все телефоны из базы данных
     phones = Phone.objects.all()
+    sort = request.GET.get('sort')
+
+    if sort == 'name':
+        # Сортировка по названию в алфавитном порядке
+        phones = phones.order_by('name')
+    elif sort == 'min_price':
+        # Сортировка по цене по возрастанию
+        phones = phones.order_by('price')
+    elif sort == 'max_price':
+        # Сортировка по цене по убыванию
+        phones = phones.order_by('-price')
+
     template = 'catalog.html'
     # Передаем список телефонов в контекст
-    context = {'phones': phones}
+    context = {'phones': phones, 'sort': sort}
     return render(request, template, context)
 
 def show_product(request, slug):
     # Получаем телефон по slug (предполагается, что slug - это уникальное поле модели Phone)
-    phone = get_object_or_404(Phone, slug=slug)  # Используем get_object_or_404 для обработки ошибок
+    # Используем get_object_or_404 для обработки ошибок
+    phone = get_object_or_404(Phone, slug=slug)
     template = 'product.html'
     # Передаем данные конкретного телефона в контекст
     context = {'phone': phone}
