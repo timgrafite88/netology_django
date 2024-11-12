bde5131edab7244b3cb9197856a413708e534352
Timur Pustoplesnov <timur.pustoplesnov@yandex.ru>
2024-11-12 23:02:25 +0300

    commit 12-11-2024

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Tag, Scope
 
-from .models import Article
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        main_count = 0
+        for form in self.forms:
+            if form.cleaned_data and form.cleaned_data.get('is_main'):
+                main_count += 1
+        if main_count != 1:
+            raise ValidationError('Должен быть один и только один основной тег.')
 
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = ScopeInlineFormset
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -1,12 +1,26 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Название тега')
+
+    class Meta:
+        verbose_name = 'Тег'
+        verbose_name_plural = 'Теги'
+
+    def __str__(self):
+        return self.name
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
+
+    # Связь многие-ко-многим через модель Scope
+    tags = models.ManyToManyField(Tag, through='Scope', related_name='articles')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +28,23 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes')
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ('article', 'tag')
+
+    def clean(self):
+        # Проверка на наличие одного и только одного основного тега
+        if self.is_main:
+            main_count = Scope.objects.filter(article=self.article, is_main=True).exclude(id=self.id).count()
+            if main_count >= 1:
+                raise ValidationError('Только один тег может быть основным.')
+
+    def __str__(self):
+        return f"{self.article.title} - {self.tag.name}"
+
